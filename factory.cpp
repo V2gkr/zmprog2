da277f6501ff551c4ed4591ed5940660e53b5bbc
V2gkr <vova.grisenko.vg@gmail.com>
2024-12-16 20:20:15 +0100

    Changed factory structure

--- factory.cpp
+++ factory.cpp
@@ -3,55 +3,111 @@
 #include "singleton.h"
 
 class Log{
-    public:
-        virtual ~Log() {}
-        virtual void print(std::string message) const = 0;
+  public:
+    virtual ~Log() {}
+    virtual void create_log(std::string message) const = 0;
 };
 
-class error_log : public Log{
-    public:
-        const std::string log_msg="Error log : ";
-        void print(std::string message) const override {
-            Logger& l1=Logger::getInstance();
-            std::string msg=log_msg+message;
-            l1.logs.push_back(msg);
-            //std::cout<<msg<< std::endl;
-        }
+class Error_Log : public Log{
+  public:
+    void create_log(std::string message) const override {
+      Logger& l1=Logger::getInstance();
+      std::string log_msg="Error log : "+message;
+      l1.logs.push_back(log_msg);
+    }
 };
 
-class info_log : public Log{
-    public:
-        const std::string log_msg="Info log : ";
-        void print(std::string message) const override {
-            Logger& l1=Logger::getInstance();
-            std::string msg=log_msg+message;
-            l1.logs.push_back(msg);
-            //std::cout<<"Info log : " << message << std::endl;
-        }
+class Info_Log : public Log{
+  public:
+    void create_log(std::string message) const override {
+      Logger& l1=Logger::getInstance();
+      std::string log_msg="Info log : "+message;
+      l1.logs.push_back(log_msg);
+    }
 };
 
-class warning_log : public Log{
-    public:
-        const std::string log_msg="Warning log : ";
-        void print(std::string message) const override {
-            Logger& l1=Logger::getInstance();
-            std::string msg=log_msg+message;
-            l1.logs.push_back(msg);
-            //std::cout<<"Warning log : " << message << std::endl;
-        }
+class Warning_Log : public Log{
+  public:
+    void create_log(std::string message) const override {
+      Logger& l1=Logger::getInstance();
+      std::string log_msg="Warning log : "+message;
+      l1.logs.push_back(log_msg);
+    }
 };
 
-void error_log_factory::fact_method(std::string message) const{
-    Log* Log1= new error_log();
-    Log1->print(message);
-}
 
-void info_log_factory::fact_method(std::string message) const{
-    Log* Log1=new info_log();
-    Log1->print(message);
-}
 
-void warning_log_factory::fact_method(std::string message) const{
-    Log* Log1=new warning_log();
-    Log1->print(message);
-}
+
+
+
+
+
+
+
+
+
+
+// class Log{
+//     public:
+//         virtual ~Log() {}
+//         virtual void print(std::string message) const = 0;
+// };
+
+// class error_log : public Log{
+//     public:
+//         const std::string log_msg="Error log : ";
+//         void print(std::string message) const override {
+//             Logger& l1=Logger::getInstance();
+//             std::string msg=log_msg+message;
+//             l1.logs.push_back(msg);
+//             //std::cout<<msg<< std::endl;
+//         }
+// };
+
+// class info_log : public Log{
+//     public:
+//         const std::string log_msg="Info log : ";
+//         void print(std::string message) const override {
+//             Logger& l1=Logger::getInstance();
+//             std::string msg=log_msg+message;
+//             l1.logs.push_back(msg);
+//             //std::cout<<"Info log : " << message << std::endl;
+//         }
+// };
+
+// class warning_log : public Log{
+//     public:
+//         const std::string log_msg="Warning log : ";
+//         void print(std::string message) const override {
+//             Logger& l1=Logger::getInstance();
+//             std::string msg=log_msg+message;
+//             l1.logs.push_back(msg);
+//             //std::cout<<"Warning log : " << message << std::endl;
+//         }
+// };
+
+
+    // switch(type){
+    //     case error:{
+    //         log_factory* factory = new error_log_factory();
+    //         factory->fact_method(message);
+    //         delete factory;
+    //         break;
+    //     }
+    //     case info:{
+    //         log_factory* factory = new info_log_factory();
+    //         factory->fact_method(message);
+    //         delete factory;
+    //         break;
+    //     }
+    //     case warning:{
+    //         log_factory* factory = new warning_log_factory();
+    //         factory->fact_method(message);
+    //         delete factory;
+    //         break;
+    //     }
+    //     default:{
+    //         std::cout<<"Invalid log type"<<std::endl;
+    //         break;
+    //     }
+    // }   
--- factory.h
+++ factory.h
@@ -3,36 +3,20 @@
 
 #include <iostream>
 
-// class Log{
+
+
+// class error_log_factory : public log_factory{
 //     public:
-//         virtual ~Log() {}
-//         virtual void print(std::string message) const = 0;
+//         void fact_method(std::string message) const override;
 // };
-//log_factory creates interface for logs
-//used by singleton logger
-
-class log_factory{
-    public:
-        virtual ~log_factory() {}
-        virtual void fact_method(std::string message) const = 0;
-        // void create_log(log_type type, std::string message) const {
-
-        // }
-
-};
-
-class error_log_factory : public log_factory{
-    public:
-        void fact_method(std::string message) const override;
-};
-class info_log_factory : public log_factory{
-    public:
-        void fact_method(std::string message) const override;
-};
-class warning_log_factory : public log_factory{
-    public:
-        void fact_method(std::string message) const override;
-};
+// class info_log_factory : public log_factory{
+//     public:
+//         void fact_method(std::string message) const override;
+// };
+// class warning_log_factory : public log_factory{
+//     public:
+//         void fact_method(std::string message) const override;
+// };
 typedef enum{
     error=1u,
     info=2u,
